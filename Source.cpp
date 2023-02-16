9d9acd971a8b0f19033ff4e41a84f41ca16b27c8
James Luke Pendleton <99436608+jp-industrial@users.noreply.github.com>
2023-02-16 18:31:43 +0000

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+#include "c:\users\james\Desktop\std_lib_facilities.h"
+
+int main()
+{
+	//binary operators
+
+	double a = 0;
+	double b = 0;
+	double c = 0;
+
+	int counter;
+	
+	++counter; //adds 1..
+	a += 1; //writes 1+a to a
+	b -= 90; //writes b-90 to b
+	c *= 2; //writes 2 * c to c,
+	c /= 2 //writes c / 2 to c. mult. and div. are known as 'scaling'.
+}
